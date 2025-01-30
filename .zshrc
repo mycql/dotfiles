5bdc56ccd215956b999d4d2beb07469fd0afc8d3
mycql <mycql04@gmail.com>
2025-01-30 16:17:35 +0800

    Add cargo

--- .zshrc
+++ .zshrc
@@ -2,3 +2,5 @@ eval "$(starship init zsh)"
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.zaliases
 source <(fzf --zsh)
+source ~/scripts/z.sh
+
--- setup_system.sh
+++ setup_system.sh
@@ -1,34 +1,34 @@
 # Install Homewbrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Set homewbrew in shell
-echo >> $HOME/.zprofile
-echo 'eval "$(/usr/local/bin/brew shellenv)"' >> $HOME/.zprofile
+echo >>$HOME/.zprofile
+echo 'eval "$(/usr/local/bin/brew shellenv)"' >>$HOME/.zprofile
 eval "$(/usr/local/bin/brew shellenv)"
 
 # Install StarShip
 curl -sS https://starship.rs/install.sh | sh
 # Setup StarShip
-echo >> ~/.zshrc
-echo eval "$(starship init zsh)" >> ~/.zshrc
+echo >>~/.zshrc
+echo eval "$(starship init zsh)" >>~/.zshrc
 eval "$(starship init zsh)"
 mkdir .config
 starship preset gruvbox-rainbow -o ~/.config/starship.toml
 
 # Further shell customizations
 brew install zsh-autosuggestions
-echo >> ~/.zshrc
-echo 'source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
+echo >>~/.zshrc
+echo 'source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >>~/.zshrc
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 brew install --cask font-jetbrains-mono
 mkdir -p ~/.config/ghostty
-echo 'font-family = "JetBrains Mono"' >> ~/.config/ghostty/config
+echo 'font-family = "JetBrains Mono"' >>~/.config/ghostty/config
 
 # Custom scripts
 mkdir scripts
 # Intstall z directory jumper
 curl https://raw.githubusercontent.com/rupa/z/refs/heads/master/z.sh -o ~/scripts/z.sh
 chmod +x ~/scripts/z.sh
-echo 'source ~/scripts/z.sh' >> ~/.zshrc
+echo 'source ~/scripts/z.sh' >>~/.zshrc
 source ~/scripts/z.sh
 
 # Install Lazyvim
@@ -36,11 +36,11 @@ brew install neovim
 brew install jesseduffield/lazygit/lazygit
 brew install fzf
 touch ~/.zaliases
-echo 'vim=nvim' >> ~/.zaliases
-echo 'lg=lazygit' >> ~/.zaliases
+echo 'vim=nvim' >>~/.zaliases
+echo 'lg=lazygit' >>~/.zaliases
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
-echo 'source <(fzf --zsh)' >> ~/.zshrc
+echo 'source <(fzf --zsh)' >>~/.zshrc
 brew install ripgrep
 brew install fd
 
@@ -56,3 +56,5 @@ mv ~/.cache/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
+# Install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
